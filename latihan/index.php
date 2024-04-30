8fff4c9c29237c4a2e9d3335135e4b0b190c0a2e
hehaj07 <hehajanah@gmail.com>
2024-05-01 01:25:11 +0700

    latihan_card_css

---
+++ latihan/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Card Practice</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+<div class="content">    
+    <div class="card">
+        <div class="card-img">
+            <img src="https://img.freepik.com/premium-photo/sailing-boats-sea-dramatic-sunset_107592-69.jpg?w=740" alt="">
+        </div>
+        <div class="card-body">
+            <h2 class="card-title">Sailing Boat</h2>
+            <p class="card-intro">
+                Dimana ada persoalan, disanalah ada jawaban. Dimana perahu berlayar disanalah air mengalir.
+            </p>
+        </div>
+    </div>
+</div>
+</body>
+</html>

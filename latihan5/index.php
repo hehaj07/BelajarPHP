d3cd8c175752f49c6c16b658bf08b6413e0aaa0e
hehaj07 <hehajanah@gmail.com>
2024-04-30 22:47:17 +0700

    latihan5_css

---
+++ latihan5/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Latihan Buttont</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <a href="#" class="btn1">Simpan</a>    
+    <a href="#" class="btn2">Pinjam</a>
+    <a href="#" class="btn3">Kembali</a>
+</body>
+</html>
